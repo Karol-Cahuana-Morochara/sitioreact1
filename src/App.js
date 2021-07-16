3c2644c65d26768fb7dcb838e18501b2f7ae1424
karol.cahuana017 <karol.cahuana017@gmail.com>
2021-07-16 07:38:53 -0500

    Version 1 MSJ nuevo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componentes from './componentes/C01componentes';
+import C02contador from './componentes/C02contador';
+<p>
+  hola... bienvenidos
+</p>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <C01componentes/>
+      <hr/>
+
+      <C02contador/>
     </div>
   );
 }
